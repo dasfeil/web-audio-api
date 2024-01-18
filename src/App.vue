3460017ea03cd71ce8aa25f901380f262f903380
NGUYEN THE VINH <vinhnt02@lgcns.com>
2024-01-18 02:07:23 -0800

    WIP

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-black w-screen h-screen">
+    <button class="bg-white rounded-xl py-2 px-5" @click="audioPlay">Test</button>
+    <input type="range" max="2" min="0" step="0.1" v-model="gainValue" />
+    <input type="range"/>
+    <Glcanvas/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+const testAudio = new Audio("clang_sfx.mp3")
+const audioContext = new window.AudioContext()
+var track = audioContext.createMediaElementSource(testAudio)
+const gainNode = audioContext.createGain()
+const gainValue = ref(1)
+
+watch(gainValue, (value) => {
+  gainNode.gain.value = value
+})
+track.connect(gainNode).connect(audioContext.destination)
+
+function audioPlay() {
+  if (audioContext.state === "suspended") audioContext.resume()
+
+  if (testAudio.paused)
+    testAudio.play()
+  else {
+    testAudio.pause()
+    testAudio.currentTime = 0
+    testAudio.play()
+  }
+}
+</script>
---
+++ src/components/GLCanvas.vue
@@ -0,0 +1,13 @@
+<template>
+  <canvas ref="canvas" width="640" height="640"></canvas>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+const canvas = ref<HTMLCanvasElement | null>(null);
+onMounted(() => {
+  const gl = canvas?.getContext("webgl");
+  gl.clearColor(255.0, 255.0, 255.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+});
+</script>
