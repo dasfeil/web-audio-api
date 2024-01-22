2bf64425c2c98b7efe09accfbc4853dd0c1b4135
Das <d4sfeil@gmail.com>
2024-01-21 19:56:01 -0800

    Grid and hover handler for canvas

--- src/components/Canvas2D.vue
+++ src/components/Canvas2D.vue
@@ -1,30 +1,64 @@
 <template>
-  <canvas ref="canvas"> </canvas>
+  <canvas ref="canvas" class="m-auto" width="640" height="640"> </canvas>
 </template>
 
 <script setup lang="ts">
 import * as THREE from "three";
 import { onMounted, ref } from "vue";
+import { DragControls, ThreeMFLoader } from "three/examples/jsm/Addons.js";
+
 const canvas = ref<HTMLCanvasElement | null>(null);
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("white");
 const camera = new THREE.OrthographicCamera();
-
+const planeGeo = new THREE.PlaneGeometry(2, 2);
 const geometry = new THREE.BoxGeometry(0.25, 0.25, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true });
 const cube = new THREE.Mesh(geometry, material);
+const plane = new THREE.Mesh(
+  planeGeo,
+  new THREE.MeshBasicMaterial({ color: 0xffffff })
+);
+const gridHelper = new THREE.GridHelper(2, 8, 0x000000, 0x000000);
+gridHelper.rotation.x = Math.PI / 2;
+camera.position.z = 3;
+gridHelper.position.z = 2;
+plane.position.z = 0
+cube.position
+  .copy(new THREE.Vector3(0, 0, 1))
+  .divideScalar(0.25)
+  .floor()
+  .multiplyScalar(0.25)
+  .addScalar(0.125);
+
 scene.add(cube);
-camera.position.z = 5;
-cube.position.x = 0
-cube.position.y = 0
+scene.add(gridHelper);
+scene.add(plane);
+
+let objects: THREE.Object3D[] = [];
+objects.push(cube);
 
 onMounted(() => {
   const renderer = new THREE.WebGLRenderer({ canvas: canvas.value! });
-  renderer.setSize(640, 640);
-  function animate() {
-    requestAnimationFrame(animate);
+
+  function render() {
     renderer.render(scene, camera);
   }
-  animate();
+
+  const controls = new DragControls(objects, camera, renderer.domElement);
+  renderer.setSize(640, 640);
+  render();
+  controls.addEventListener("hoveron", function (event) {
+    if (event.object instanceof THREE.Mesh) {
+      event.object.material.opacity = 0.5;
+      render();
+    }
+  });
+
+  controls.addEventListener("hoveroff", function (event) {
+    if (event.object instanceof THREE.Mesh) {
+      event.object.material.opacity = 1;
+      render();
+    }
+  });
 });
 </script>
